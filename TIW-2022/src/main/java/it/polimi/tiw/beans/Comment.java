f336a5acc58570e1276dbfcd74cbb9d61972909e
MatteoPancini <matteo.pancini@mail.polimi.it>
2022-04-13 16:06:01 +0200

    Comment Bean

---
+++ TIW-2022/src/main/java/it/polimi/tiw/beans/Comment.java
@@ -0,0 +1,73 @@
+package it.polimi.tiw.beans;
+
+public class Comment {
+	
+	private int commentID;
+	private int imageID;
+	private int albumID;
+	private int userID;
+	private String text;
+	
+
+	public Comment(int commentID, int imageID, int albumID, int userID, String text) {
+		this.commentID = commentID;
+		this.imageID = imageID;
+		this.albumID = albumID;
+		this.userID = userID;
+		this.text = text;
+	}
+
+
+	public int getCommentID() {
+		return commentID;
+	}
+
+
+	public void setCommentID(int commentID) {
+		this.commentID = commentID;
+	}
+
+
+	public int getImageID() {
+		return imageID;
+	}
+
+
+	public void setImageID(int imageID) {
+		this.imageID = imageID;
+	}
+
+
+	public int getAlbumID() {
+		return albumID;
+	}
+
+
+	public void setAlbumID(int albumID) {
+		this.albumID = albumID;
+	}
+
+
+	public int getUserID() {
+		return userID;
+	}
+
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+
+	public String getText() {
+		return text;
+	}
+
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+	
+
+}
